ae427bca92fc86fa0be831e622046c44a6cd2998
Lucas Cunha <lucasviana112@gmail.com>
2021-03-23 14:37:28 -0300

    new routes: auth and auth validate (public and private route configuration)

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import SessionController from './controllers/SessionController';
 import UsersController from './controllers/UsersController';
+import ensureAuthenticated from './middlewares/ensureAuthenticated';
 
 const router = Router();
 
 const usersController = new UsersController();
+const sessionController = new SessionController();
 
 router.post('/users', usersController.store);
+router.post('/auth', sessionController.store);
+
+router.use(ensureAuthenticated);
+router.put('/auth/validate', sessionController.update);
 
 export default router;
