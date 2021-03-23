88e121db45b30e544841aa59cac81be3f5ee8846
Lucas Cunha <lucasviana112@gmail.com>
2021-03-23 14:36:46 -0300

    adjustment method for create new user

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,8 +1,35 @@
 import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+import { AppError } from '../errors/AppError';
+import UsersRepository from '../repositories/UsersRepository';
 
 class UsersController {
   async store(request: Request, response: Response) {
-    return response.send('test');
+    const { name, lastname, email, password } = request.body;
+
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) {
+      throw new AppError('User already exists!');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const code = Math.floor(Math.random() * 99999);
+    const user = usersRepository.create({
+      name,
+      lastname,
+      email,
+      password: hashedPassword,
+      code,
+      status: 'common',
+    });
+    await usersRepository.save(user);
+
+    return response.status(201).json(user);
   }
 }
 
