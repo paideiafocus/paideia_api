2ffbe9c5ad599a5027d514d674a11b63c5d3ab06
Lucas Cunha <lucasviana112@gmail.com>
2021-03-23 00:59:08 -0300

    improve server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
+import 'reflect-metadata';
 import express from 'express';
+import 'express-async-errors';
+import createConnection from './database';
+import router from './routes';
+// AppError
 
+createConnection();
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server open...'));
