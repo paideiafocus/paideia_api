489f30429efc8e0a3cc00ae2821ed42ca56497cc
Lucas Cunha <lucasviana112@gmail.com>
2021-03-23 14:35:04 -0300

    create Session controler (store and update code)

---
+++ src/controllers/SessionController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { AppError } from '../errors/AppError';
+import UsersRepository from '../repositories/UsersRepository';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+class SessionController {
+  async store(request: Request, response: Response) {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user, token } = await authenticateUser.execute({ email, password });
+
+    return response.json({ user, token });
+  }
+
+  async update(request: Request, response: Response) {
+    const {
+      userToken: { id },
+      code,
+    } = request.body;
+
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findOne({ id, code });
+
+    if (!user) {
+      throw new AppError('Invalid code!');
+    }
+
+    await usersRepository.update({ id }, { status: 'active' });
+
+    return response.json({
+      message: 'code updated successfully',
+      status: 'active',
+    });
+  }
+}
+
+export default SessionController;
