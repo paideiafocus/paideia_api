3cf14ef70d7e5bef963b4aacd90e11c70423bde4
Lucas Cunha <lucasviana112@gmail.com>
2021-03-23 00:59:49 -0300

    create users migration

---
+++ src/database/migrations/1616467707196-MigrationName.ts
@@ -0,0 +1,64 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class MigrationName1616467707196 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'code',
+            type: 'int',
+          },
+          {
+            name: 'enrollment',
+            type: 'int',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'lastname',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'presence',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
