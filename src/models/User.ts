f1266deacfe1f59fcacb89f071b57af25d8422f4
Lucas Cunha <lucasviana112@gmail.com>
2021-03-23 00:56:06 -0300

    create User model

---
+++ src/models/User.ts
@@ -0,0 +1,46 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  code: number;
+
+  @Column()
+  enrollment: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  lastname: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  presence: string;
+
+  @Column()
+  status: string;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export default User;
