c4cc54eabd28863afdb51db4d97d3885c8e03f45
Lucas Cunha <lucasviana112@gmail.com>
2021-03-23 14:34:06 -0300

    create autentication user service

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,48 @@
+import { getCustomRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+import { AppError } from '../errors/AppError';
+
+import User from '../models/User';
+import UsersRepository from '../repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
