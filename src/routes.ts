be6ce5df86b37b53d702865eaf4c95c9a26a25a0
Lucas Cunha <lucasviana112@gmail.com>
2021-03-25 22:23:02 -0300

    route form for example

--- src/routes.ts
+++ src/routes.ts
@@ -13,5 +13,48 @@ router.post('/auth', sessionController.store);
 
 router.use(ensureAuthenticated);
 router.put('/auth/validate', sessionController.update);
+router.post('/socioeconomic', (request, response) => {
+  // const mailService = new MailService();
+  //   const npsPath = resolve(
+  //     __dirname,
+  //     '..',
+  //     'views',
+  //     'emails',
+  //     'userRegistration.hbs',
+  //   );
+  //   const filePath = resolve(
+  //     __dirname,
+  //     '..',
+  //     'views',
+  //     'emails',
+  //     'termo_responsabilidade.pdf',
+  //   );
+  //   const variables = {
+  //     name,
+  //     enrollment: user.enrollment,
+  //     description: 'lista regular',
+  //   };
+  //   const variables2 = {
+  //     name,
+  //     enrollment: user.enrollment,
+  //     description:
+  //       'lista de espera (lembramos que você precisa participar de todas as etapas igualmente)',
+  //   };
+  //   await mailService.execute(
+  //     email,
+  //     'Confirmação de inscrição',
+  //     variables,
+  //     npsPath,
+  //     filePath,
+  //   );
+  //   await mailService.execute(
+  //     email,
+  //     'Confirmação de inscrição',
+  //     variables2,
+  //     npsPath,
+  //     filePath,
+  //   );
+  return response.json({ message: 'okay' });
+});
 
 export default router;
