62c7b37c4c139550de18783821f23fe673d9a7cc
Lucas Cunha <lucasviana112@gmail.com>
2021-03-31 19:22:37 -0300

    create Recover Password controller

---
+++ src/controllers/RecoverPasswordController.ts
@@ -0,0 +1,113 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { resolve } from 'path';
+import { verify } from 'jsonwebtoken';
+import { hash } from 'bcryptjs';
+
+import { AppError } from '../errors/AppError';
+import UsersRepository from '../repositories/UsersRepository';
+import PasswordRecoveryService from '../services/PasswordRecoveryService';
+import MailService from '../services/MailService';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+class RecoverPasswordController {
+  async store(request: Request, response: Response) {
+    const { email } = request.body;
+
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findOne({ email });
+
+    if (!user) {
+      throw new AppError('E-mail not found!');
+    }
+
+    const passwordRecoveryService = new PasswordRecoveryService();
+
+    const { token } = await passwordRecoveryService.execute({
+      userId: user.id,
+    });
+
+    const mailService = new MailService();
+
+    const templatePath = resolve(
+      __dirname,
+      '..',
+      'views',
+      'emails',
+      'userRecoverPassword.hbs',
+    );
+    const variables = {
+      name: user.name,
+      link: `${process.env.SITE_BASE_URL}/${token}`,
+    };
+
+    await mailService.execute({
+      to: email,
+      subject: 'Esqueceu sua senha?',
+      variables,
+      templatePath,
+    });
+
+    return response.json({ token });
+  }
+
+  async show(request: Request, response: Response) {
+    const { token } = request.body;
+    const secret = process.env.TOKEN_SECRET_RECOVERY;
+
+    try {
+      const decode = verify(token, secret);
+      const { sub } = decode as TokenPayload;
+      const usersRepository = getCustomRepository(UsersRepository);
+
+      const user = await usersRepository.findOne({ id: sub });
+
+      if (!user) {
+        throw new AppError('Invalid token!', 401);
+      }
+    } catch {
+      throw new AppError('Invalid token!', 401);
+    }
+
+    return response.json({
+      message: 'Valid token',
+    });
+  }
+
+  async update(request: Request, response: Response) {
+    const { newPassword, confirmNewPassword, token } = request.body;
+    const secret = process.env.TOKEN_SECRET_RECOVERY;
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    try {
+      const decode = verify(token, secret);
+      const { sub } = decode as TokenPayload;
+      const user = await usersRepository.findOne({ id: sub });
+
+      if (!user) {
+        throw new AppError('Invalid token!', 401);
+      }
+
+      const hashedPassword = await hash(newPassword, 8);
+      await usersRepository.update({ id: sub }, { password: hashedPassword });
+    } catch {
+      throw new AppError('Invalid token!', 401);
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      throw new AppError('Password not match!');
+    }
+
+    return response.json({
+      message: 'Password successfully changed',
+    });
+  }
+}
+
+export default RecoverPasswordController;
