8d94f0feb53a0738b716b5a5521128319f5b9320
Lucas Cunha <lucasviana112@gmail.com>
2021-06-03 23:39:21 -0300

    adjustment the query for subscribers infos

--- src/controllers/SubscribersController.ts
+++ src/controllers/SubscribersController.ts
@@ -19,9 +19,18 @@ class SubscribersController {
 
     const candidatesRepository = getCustomRepository(CandidatesRepository);
 
-    const query = `SELECT * FROM candidates INNER JOIN users ON candidates.user_id = users.id
-      WHERE users.status = 'subscriber' OR users.status = 'waiting'`;
+    const userColumns = `u.code, u.enrollment, u.name, u.lastname, u.email, u.status`;
+    const candidateColumns = `c.id as candidate_id, c.citizen, c.cpf, c.course, c.birth_date, c.rg, c.phone1`;
+    const socioeconomicColumns = `
+      s.cinema, s.sports, s.exam_entrance, s.elementary_school, s.age, s.informed, s.internet, s.internet_activity, s.reading_activity, s.read, s.read_qtd, s.place, s.high_school,
+      s.live_with_friend, s.live_with_grandfather, s.live_with_couple, s.live_with_mother, s.live_with_father, s.live_with_alone, s.live_qtd, s.live_time, s.live_type, s.study_why,
+      s.music, s.no_activity, s.genre, s.tv, s.work_candidate, s.work_father, s.transport`;
 
+    const query = `
+      SELECT ${userColumns}, ${candidateColumns}, ${socioeconomicColumns} FROM candidates c
+      INNER JOIN users u ON c.user_id = u.id
+      INNER JOIN socioeconomic s ON s.user_id = u.id
+      WHERE u.status = 'subscriber' OR u.status = 'waiting'`;
     const subscribers = await candidatesRepository.query(query);
 
     return response.json(subscribers);
