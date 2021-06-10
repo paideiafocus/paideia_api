babb540eedcc081704b5fa66929e62c68d73689d
Lucas Cunha <lucasviana112@gmail.com>
2021-06-10 14:30:48 -0300

    change ormconfig paths for tests

--- ormconfig.js
+++ ormconfig.js
@@ -11,10 +11,10 @@ module.exports = {
   port,
   username,
   password,
-  migrations: ["./src/database/migrations/**.ts"],
-  entities: ["./src/models/**.ts"],
+  migrations: ["./build/src/database/migrations/**.js"],
+  entities: ["./build/src/models/**.js"],
   logging: false,
   cli: {
-    "migrationsDir": "./src/database/migrations"
+    "migrationsDir": "./build/src/database/migrations"
   }
 }
