6abf150cfb206e366901cd84f1252d1724744539
Lucas Cunha <lucasviana112@gmail.com>
2021-06-11 13:07:58 -0300

    added new collumns on the admin query (from socioeconomic table)

--- src/controllers/SubscribersController.ts
+++ src/controllers/SubscribersController.ts
@@ -23,9 +23,12 @@ class SubscribersController {
     const userColumns = `u.id as user_id, u.code, u.enrollment, u.name, u.lastname, u.email, u.status`;
     const candidateColumns = `c.id as candidate_id, c.citizen, c.cpf, c.course, c.birth_date, c.rg, c.phone1`;
     const socioeconomicColumns = `
-      s.cinema, s.sports, s.exam_entrance, s.elementary_school, s.age, s.informed, s.internet, s.internet_activity, s.reading_activity, s.read, s.read_qtd, s.place, s.high_school,
-      s.live_with_friend, s.live_with_grandfather, s.live_with_couple, s.live_with_mother, s.live_with_father, s.live_with_alone, s.live_qtd, s.live_time, s.live_type, s.study_why,
-      s.music, s.no_activity, s.genre, s.tv, s.work_candidate, s.work_father, s.transport`;
+      s.cinema, s.sports, s.exam_entrance, s.elementary_school, s.age, s.informed, s.internet, s.internet_activity, s.reading_activity,
+      s.read, s.read_qtd, s.place, s.high_school, s.live_with_friend, s.live_with_grandfather, s.live_with_couple, s.live_with_mother,
+      s.live_with_father, s.live_with_alone, s.live_qtd, s.live_time, s.live_type, s.study_why, s.music, s.no_activity, s.genre, s.tv,
+      s.work_candidate, s.work_father, s.transport, s.participate_ead, s.understand_ead, s.local_quarantine, s.unprotected_people,
+      s.responsibilities, s.smartphone, s.internet_smartphone, s.internet_smartphone_limit, s.equips, s.pc_shared, s.study_local,
+      s.internet_quality`;
 
     const query = `
       SELECT ${userColumns}, ${candidateColumns}, ${socioeconomicColumns} FROM candidates c
@@ -54,9 +57,12 @@ class SubscribersController {
     const userColumns = `u.id as user_id, u.code, u.enrollment, u.name, u.lastname, u.email, u.status`;
     const candidateColumns = `c.id as candidate_id, c.citizen, c.cpf, c.course, c.birth_date, c.rg, c.phone1`;
     const socioeconomicColumns = `
-      s.cinema, s.sports, s.exam_entrance, s.elementary_school, s.age, s.informed, s.internet, s.internet_activity, s.reading_activity, s.read, s.read_qtd, s.place, s.high_school,
-      s.live_with_friend, s.live_with_grandfather, s.live_with_couple, s.live_with_mother, s.live_with_father, s.live_with_alone, s.live_qtd, s.live_time, s.live_type, s.study_why,
-      s.music, s.no_activity, s.genre, s.tv, s.work_candidate, s.work_father, s.transport`;
+      s.cinema, s.sports, s.exam_entrance, s.elementary_school, s.age, s.informed, s.internet, s.internet_activity, s.reading_activity,
+      s.read, s.read_qtd, s.place, s.high_school, s.live_with_friend, s.live_with_grandfather, s.live_with_couple, s.live_with_mother,
+      s.live_with_father, s.live_with_alone, s.live_qtd, s.live_time, s.live_type, s.study_why, s.music, s.no_activity, s.genre, s.tv,
+      s.work_candidate, s.work_father, s.transport, s.participate_ead, s.understand_ead, s.local_quarantine, s.unprotected_people,
+      s.responsibilities, s.smartphone, s.internet_smartphone, s.internet_smartphone_limit, s.equips, s.pc_shared, s.study_local,
+      s.internet_quality`;
 
     const query = `
       SELECT ${userColumns}, ${candidateColumns}, ${socioeconomicColumns} FROM candidates c
