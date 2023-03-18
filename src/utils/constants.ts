8d19c5f6c25455a2cb1a902ba518fb272f6f8834
Lucas Cunha <lucas.cunha@luizalabs.com>
2023-03-17 23:19:14 -0300

    chore: change waiting list limit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const defaultEnrollment = 2209;
 export const totalRegularSubscribers = 100;
-export const totalSubscribers = 150;
+export const totalSubscribers = 130;
 export const limitRegularSubscribers = totalRegularSubscribers - 1;
 export const limitWaitingSubscribers = totalSubscribers - 1;
