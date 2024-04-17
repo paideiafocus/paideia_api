ad1eaedec498bf7f35113c0eb7a171f7a0cb2095
Lucas Cunha <lucas.cunha@luizalabs.com>
2024-04-16 21:49:42 -0300

    feat: update inscription 1 sem 2024

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const defaultEnrollment = 2209;
-export const totalRegularSubscribers = 100;
-export const totalSubscribers = 130;
+export const totalRegularSubscribers = 80;
+export const totalSubscribers = 120;
 export const limitRegularSubscribers = totalRegularSubscribers - 1;
 export const limitWaitingSubscribers = totalSubscribers - 1;
